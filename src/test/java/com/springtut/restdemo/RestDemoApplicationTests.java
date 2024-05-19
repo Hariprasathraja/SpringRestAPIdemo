ed1c1b59319707ce49c5071bbc82e605091c985b
Hariprasathraja <hariprasathraja6023@gmail.com>
2024-05-19 18:33:59 +0530

    Initial commit

---
+++ src/test/java/com/springtut/restdemo/RestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtut.restdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
